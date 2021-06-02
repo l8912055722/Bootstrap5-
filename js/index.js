330b854747c5dba6aba3c5fbbcf12a5214cdc89a
l8912055722 <66676628+l8912055722@users.noreply.github.com>
2021-06-02 11:22:26 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,9 +4,30 @@ var tooltipTriggerList = [].slice.call(
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
-  var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+
+let subNav = document.getElementById('subNav'); 
+let sponsorBtn = document.getElementById('sponsorBtn');
+
+function triggerSubNav(){ 
+  if(window.pageYOffset > 1900){
+    subNav.classList.remove('position-sticky','top-0','zIndexTop');
+		// 當 window.pageYOffset > 1900 條件成立時，
+    // 移除 subNav 的 'position-sticky','top-0','zIndexTop' 三個 class 
+  }else{
+    subNav.classList.add('position-sticky','top-0','zIndexTop');
+	 //  當 window.pageYOffset > 1900 條件不成立時，
+    // 把 subNav 加上 'position-sticky','top-0','zIndexTop' 三個 class 
+  }
+}
+window.addEventListener("scroll",triggerSubNav);
+
+function triggerSponsorBtn(){
+  if(window.pageYOffset > 400 && window.pageYOffset < 1800){
+    sponsorBtn.classList.remove('d-none');
+  }else{
+    sponsorBtn.classList.add('d-none');
+  }
+}
+window.addEventListener("scroll",triggerSponsorBtn);
+
